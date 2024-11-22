13f6f69ad69d530dca9a8c21dbb76b9d95a8bb33
getFrontend <sergey10goals@gmail.com>
2024-11-23 00:32:58 +0200

    updated initial RootLayout

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
-import { StatusBar, Text } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-blue-200">
       <Text className="text-blue-500 text-8xl text-center font-bold">Uber</Text>
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
