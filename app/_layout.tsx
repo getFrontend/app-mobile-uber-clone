b3996dc34434c27e0bcd4bde161e1260ff892839
getFrontend <sergey10goals@gmail.com>
2024-11-24 18:12:44 +0200

    updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,10 +9,10 @@ import { LogBox } from "react-native";
 
 import { tokenCache } from "@/lib/auth";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function RootLayout() {
   const [loaded] = useFonts({
